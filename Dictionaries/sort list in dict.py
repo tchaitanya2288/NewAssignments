5f06d53364de631c46fea5d06ac13afbbbda4109
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-10 18:48:26 -0400

    string_specified_char

---
+++ Dictionaries/sort list in dict.py
@@ -0,0 +1,8 @@
+num = {'n1': ['python','pycharm','anaconda','django']}
+
+SortedLogic = {x: sorted(y) for x, y in num.items()}
+print("sorted list")
+print(SortedLogic)
+print("un sorted list")
+print(num)
+
