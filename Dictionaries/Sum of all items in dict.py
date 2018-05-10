2016b24e319687d574277bc48cf7edc494dfdd13
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-10 17:13:52 -0400

    sum of all items in dict

---
+++ Dictionaries/Sum of all items in dict.py
@@ -0,0 +1,10 @@
+A = {'Rent' : 5000,
+     "Current_Bill" : 1000,
+     "Groceries" : 4000,
+     "Expenses" : 5000}
+
+print(A,type(A),id(A))
+print(' ')
+sum(A.values())
+print(" ")
+print(sum(A.values()))
