cff89806a67e4e7330e1190dcf6bd6df3b3d65cd
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-13 20:45:55 -0400

    lastpart of string

---
+++ Strings/get last part of string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+str = 'Manual-Testing/Selenium-Testing/Python.3.6.5'
+
+print(str)
+print(' ')
+print(str.rsplit('/',1)[0])
+print(' ')
+print(str.rsplit('-',1)[0])
+print('')
+print(str.rsplit('P',1)[0])
+print(' ')
+print(str.rsplit('T',1)[0])
