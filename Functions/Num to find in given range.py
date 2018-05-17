b03f0d5bdcd7ed89ef020872fd60815704dda594
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-05-16 21:48:26 -0400

    Find range in num

---
+++ Functions/Num to find in given range.py
@@ -0,0 +1,9 @@
+#!/user/bin/python
+def range_num(n):
+    if n in range(1,50):
+        print( "%s is in the range"%str(n))
+    else :
+        print("%s is outside the given range"%str(n))
+
+range_num(75)
+range_num(35)
